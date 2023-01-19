c47885984c2e64224be3f149230f6871605c61f8
Motzkiste <motzkiste@proton.me>
2023-01-20 00:24:41 +0100

    fixed installer

--- installer.lua
+++ installer.lua
@@ -1,11 +1,11 @@
 local version = "1.0.0.alpha1"
+print("INSTALLER "..version)
+
 local main_url = "https://raw.githubusercontent.com/Motzkiste/ComputerCraft-Scripts/main/"
 
 local function download(file)
   shell.run("wget "..main_url..file.." "..file)
 end
-
-print("INSTALLER " ~ version)
 
 print("creating dirs...")
 shell.run("mkdir ReactorController/reactors")
@@ -15,3 +15,8 @@ download("ReactorController/reactor_class.lua")
 download("ReactorController/reactors/big.lua")
 download("ReactorController/reactors/bigger.lua")
 download("ReactorController/reactors/extreme.lua")
+
+print()
+print("all done!")
+
+sleep(2)
